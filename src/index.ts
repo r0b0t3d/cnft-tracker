9960d6c05ebf95757ff6b6d26ca943d8ecbcdda0
Tuan Luong <tuanluong.it@gmail.com>
2022-02-23 15:44:13 +0700

    add interval

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,43 @@
-import { fork } from 'child_process';
+import { fork } from "child_process";
+const yargs = require("yargs");
+
+const argv = yargs
+  .command("project", "which project", {
+    year: {
+      description: "project",
+      alias: "prj",
+      type: "string",
+    },
+  })
+  .option("search", {
+    alias: "s",
+    description: "search value",
+    type: "string",
+  })
+  .option("price", {
+    alias: "p",
+    description: "search value",
+    type: "number",
+  })
+  .option("interval", {
+    alias: "i",
+    description: "interval time",
+    type: "number",
+  })
+  .help()
+  .alias("help", "h").argv;
 
 const args = process.argv.slice(2);
 
-function main() {
-    const [project, ...otherArgs] = args;
-    fork(`src/projects/${project}.ts`, otherArgs);
-}
+// console.log({ argv: argv });
 
+function main() {
+  const [project, ...otherArgs] = args;
+  fork(`src/projects/${project}.ts`, otherArgs);
+  setInterval(() => {
+    console.log("Start:", otherArgs);
+    fork(`src/projects/${project}.ts`, otherArgs);
+  }, 30000);
+}
 
 main();
--- src/projects/unboundedearth.ts
+++ src/projects/unboundedearth.ts
@@ -3,57 +3,37 @@ import jpgStore from "../services/jpgStore";
 
 const args = process.argv.slice(2);
 
+function isCoordValid(season: string, x: number, y: number) {
+  if (season.toLowerCase() === "summer") {
+    return x < 80;
+  }
+  if (season.toLowerCase() === "winter") {
+    return x < 80 && y > -122;
+  }
+  return true;
+}
+
 async function run() {
   let cursor = 0;
   const searchText = args[0];
   const maxPrice = args[1] ? parseInt(args[1]) : 200; // in ADA
   let currentPrice = 0;
   while (true) {
-    const cnftData = await jpgStore.getCnfts("unboundedearth", {
-      cursor,
-    });
-    cursor = cnftData.cursor;
-    cnftData.nfts.forEach((nft) => {
-      const meta = nft.onchain_metadata;
-      const season = meta["---SEASON---"];
-      const price = parseInt(nft.price_lovelace, 0);
-      if (
-        season.toLowerCase().includes(searchText.toLowerCase()) &&
-        price < maxPrice * 1000000
-      ) {
-        const link = `https://www.jpg.store/asset/${nft.asset_id}`;
-        console.log({
-          Season: season,
-          Coords: meta["---X---"] + "," + meta["---Y---"],
-          Price: `${price / 1000000}₳`,
-          Link: link,
-        });
-      }
-      currentPrice = Math.max(currentPrice, price);
-    });
-
-    if (!cnftData.hasMore || currentPrice > maxPrice * 1000000) {
-      break;
-    }
-  }
-
-  currentPrice = 0;
-  let page = 1;
-  while (true) {
-    const { results, count } = await cnftio.getCnfts("unboundedearth", {
-      page,
-    });
-    results.forEach((nft) => {
-      const price = parseInt(nft.price, 0);
-      const asset = nft.assets[0];
-      if (asset) {
-        const meta = asset.metadata;
+    try {
+      const cnftData = await jpgStore.getCnfts("unboundedearth", {
+        cursor,
+      });
+      cursor = cnftData.cursor;
+      cnftData.nfts.forEach((nft) => {
+        const meta = nft.onchain_metadata;
         const season = meta["---SEASON---"];
+        const price = Number(nft.price_lovelace);
         if (
           season.toLowerCase().includes(searchText.toLowerCase()) &&
-          price < maxPrice * 1000000
+          price < maxPrice * 1000000 &&
+          isCoordValid(season, Number(meta["---X---"]), Number(meta["---Y---"]))
         ) {
-          const link = `https://cnft.io/token/${nft._id}`;
+          const link = `https://www.jpg.store/asset/${nft.asset_id}`;
           console.log({
             Season: season,
             Coords: meta["---X---"] + "," + meta["---Y---"],
@@ -61,13 +41,52 @@ async function run() {
             Link: link,
           });
         }
+        currentPrice = Math.max(currentPrice, price);
+      });
+
+      if (!cnftData.hasMore || currentPrice > maxPrice * 1000000) {
+        break;
       }
-      currentPrice = Math.max(currentPrice, price);
-    });
-    if (count === 0 || currentPrice > maxPrice * 1000000) {
+    } catch (error) {
       break;
     }
-    page += 1;
+  }
+
+  currentPrice = 0;
+  let page = 1;
+  while (true) {
+    try {
+      const { results, count } = await cnftio.getCnfts("unboundedearth", {
+        page,
+      });
+      results.forEach((nft) => {
+        const price = parseInt(nft.price, 0);
+        const asset = nft.assets[0];
+        if (asset) {
+          const meta = asset.metadata;
+          const season = meta["---SEASON---"];
+          if (
+            season.toLowerCase().includes(searchText.toLowerCase()) &&
+            price < maxPrice * 1000000
+          ) {
+            const link = `https://cnft.io/token/${nft._id}`;
+            console.log({
+              Season: season,
+              Coords: meta["---X---"] + "," + meta["---Y---"],
+              Price: `${price / 1000000}₳`,
+              Link: link,
+            });
+          }
+        }
+        currentPrice = Math.max(currentPrice, price);
+      });
+      if (count === 0 || currentPrice > maxPrice * 1000000) {
+        break;
+      }
+      page += 1;
+    } catch (error) {
+      break;
+    }
   }
 }
 
